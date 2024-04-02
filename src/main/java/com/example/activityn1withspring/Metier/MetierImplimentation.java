b4949dd7e221fe6d5225a875e83b04b124654c74
Yahya Saadaoui <saadaouiyahya706@gmail.com>
2024-04-02 12:43:14 +0000

    Adding new Annotation Way

--- src/main/java/com/example/activityn1withspring/Metier/MetierImplimentation.java
+++ src/main/java/com/example/activityn1withspring/Metier/MetierImplimentation.java
@@ -2,10 +2,12 @@ package com.example.activityn1withspring.Metier;
 
 import Dao.IDao;
 import Metier.IMetier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("metier")
 public class MetierImplimentation implements IMetier {
+    @Autowired
     private IDao dao;
     public void setDao(IDao dao) {
         this.dao = dao;
---
+++ src/main/java/com/example/activityn1withspring/presentation/PresentationSpringAnnotation.java
@@ -0,0 +1,13 @@
+package com.example.activityn1withspring.presentation;
+
+import Metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class PresentationSpringAnnotation {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com");
+        IMetier metier = context.getBean(IMetier.class);
+        System.out.println("Results   :  " + metier.calcul());
+    }
+}
